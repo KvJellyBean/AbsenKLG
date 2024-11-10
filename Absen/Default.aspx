c378224794c12fbcfaec111aee6448084e735ce2
KvJellyBean <kevinnatan91@gmail.com>
2024-11-10 10:24:34 +0700

    Add project files.

---
+++ Absen/Default.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Absen._Default" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Absensi</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 120px;
+        }
+        #datepicker {
+            width: 216px;
+        }
+    </style>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
+    <script>
+      $( function() {
+          $("#datepicker").datepicker({
+              dateFormat: 'dd/mm/yy',
+              showOn: 'both',
+              buttonText: 'Calendar',
+              changeMonth: true,
+              changeYear: true,
+          });
+      } );
+    </script>
+</head>
+<body>
+    <h1>Absensi</h1>
+    <form id="FormAbsensi" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">NIK - Nama :</td>
+                <td>
+                    <asp:DropDownList ID="DropDownNikNama" runat="server"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Tgl Absen</td>
+                <td>
+                   <asp:TextBox ID="datepicker" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                    <asp:Button ID="SaveButton" runat="server" Text="Save" OnClick="saveButton_Click" />
+                </td>
+            </tr>
+        </table>
+
+        <asp:GridView ID="GridView1" runat="server" style="margin-top: 25px" Width="350px" OnRowDataBound="GridView1_RowDataBound">
+        </asp:GridView>
+
+        <asp:GridView ID="GridView2" runat="server" style="margin-top: 25px; white-space: nowrap" >
+        </asp:GridView>
+        <asp:GridView ID="GridView3" runat="server" style="margin-top: 25px" Width="350px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Absen/Default.aspx.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+using System.Security.Policy;
+
+
+namespace Absen
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=NATANAELS\\SQLEXPRESS01;Initial Catalog=tesklg;Integrated Security=True;TrustServerCertificate=True");
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+             {
+                fillDropDown();
+                printGridAbsenEmployee();
+                printGridAbsenList();
+                printGridAbsenBulanan();
+            }
+        }
+
+        public DataTable getEmployee()
+        {
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("SELECT *, (Nama + ' - ' + NIK) AS NamaNik FROM employee", con);
+                cmd.CommandType = CommandType.Text;
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+            } catch (Exception ex)
+            {
+                throw new Exception("Error fetching employee data: " + ex.Message);
+            } finally
+            {
+                con.Close();
+            }
+           
+            return dt;
+        }
+
+        public DataTable getAbsenEmployee()
+        {
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("SELECT e.NIK, e.Nama, a.Tanggal_Absen as [Tanggal Absen]  FROM employee AS e JOIN absen AS a ON e.NIK = a.NIK", con);
+                cmd.CommandType = CommandType.Text;
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+            } catch (Exception ex)
+            {
+                throw new Exception("Error fetching employee's attendance data: " + ex.Message);
+            } finally
+            {
+                con.Close();
+            }
+
+            return dt;
+        }
+
+        public DataTable getAbsenList()
+        {
+            SqlCommand dateCmd = new SqlCommand("SELECT DISTINCT Tanggal_Absen FROM absen ORDER BY Tanggal_Absen ASC", con);
+            SqlDataAdapter dateDa = new SqlDataAdapter(dateCmd);
+            DataTable dateTable = new DataTable();
+            dateDa.Fill(dateTable);
+
+            string dateColumns = string.Join(", ", dateTable.AsEnumerable()
+                                    .Select(row => $"MAX(CASE WHEN CONVERT(VARCHAR(10), a.Tanggal_Absen, 105) = '{Convert.ToDateTime(row["Tanggal_Absen"]).ToString("dd-MM-yyyy")}' THEN 'X' ELSE '' END) AS [{Convert.ToDateTime(row["Tanggal_Absen"]).ToString("dd-MM-yyyy")}]"));
+
+            string query = $@"
+                SELECT e.NIK, e.Nama, 
+                       {dateColumns},
+                       COUNT(a.Tanggal_Absen) AS Total
+                FROM employee AS e
+                LEFT JOIN absen AS a ON e.NIK = a.NIK
+                GROUP BY e.NIK, e.Nama;
+            ";
+
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+            } catch (Exception ex)
+            {
+                throw new Exception("Error fetching attendace list data: " + ex.Message);
+            } finally
+            {
+                con.Close();
+            }
+
+            return dt;
+        }
+
+        public DataTable getAbsenBulanan()
+        {
+            SqlCommand dateCmd = new SqlCommand("SELECT DISTINCT CONVERT(VARCHAR(6), Tanggal_Absen, 112) AS Tanggal_Absen FROM absen", con);
+            SqlDataAdapter dateDa = new SqlDataAdapter(dateCmd);
+            DataTable dateTable = new DataTable();
+            dateDa.Fill(dateTable);
+
+            string dateColumns = string.Join(", ", dateTable.AsEnumerable()
+                                    .Select(row => $"COUNT(CASE WHEN CONVERT(VARCHAR(6), a.Tanggal_Absen, 112) = '{row["Tanggal_Absen"]}' THEN 1 ELSE NULL END) AS [{row["Tanggal_Absen"]}]"));
+
+            string query = $@"
+                SELECT e.NIK, e.Nama, 
+                       {dateColumns},
+                       COUNT(a.Tanggal_Absen) AS Total
+                FROM employee AS e
+                LEFT JOIN absen AS a ON e.NIK = a.NIK
+                GROUP BY e.NIK, e.Nama;
+            ";
+
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error fetching employee monthly attendance data: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return dt;
+        }
+
+        private void fillDropDown()
+        {
+            DropDownNikNama.DataSource = getEmployee();
+            DropDownNikNama.DataTextField = "NamaNik";
+            DropDownNikNama.DataValueField = "NIK";
+            DropDownNikNama.DataBind();
+        }
+
+        private void printGridAbsenEmployee()
+        {
+            GridView1.DataSource = getAbsenEmployee();
+            GridView1.DataBind();
+        }
+
+        private void printGridAbsenList()
+        {
+            GridView2.DataSource = getAbsenList();
+            GridView2.DataBind();
+        }
+
+        private void printGridAbsenBulanan()
+        {
+            GridView3.DataSource = getAbsenBulanan();
+            GridView3.DataBind();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                var tanggalAbsen = DataBinder.Eval(e.Row.DataItem, "Tanggal Absen");
+
+                if (tanggalAbsen != DBNull.Value)
+                {
+                    DateTime date = Convert.ToDateTime(tanggalAbsen);
+                    e.Row.Cells[2].Text = date.ToString("dd/MM/yyyy"); 
+                }
+            }
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string nik = DropDownNikNama.SelectedValue;
+            string tanggalText = datepicker.Text;
+
+            try
+            {
+                DateTime tanggalAbsen = DateTime.ParseExact(tanggalText, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                if (isDuplicateAttendance(nik, tanggalAbsen))
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script",
+                        "alert('Karyawan sudah absen di tanggal tersebut!'); window.location.href = '/';", true);
+                    return;
+                }
+
+                saveData(nik, tanggalAbsen);
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Data berhasil ditambahkan'); window.location.href = '/';", true);
+            }
+            catch (FormatException ex)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Format tanggal tidak valid. Harap masukkan tanggal dengan format dd/MM/yyyy.'); window.location.href = '/';", true);
+            }
+        }
+
+        public void saveData(string nik, DateTime tanggalAbsen)
+        {
+            string query = "INSERT INTO absen (NIK, Tanggal_Absen) VALUES (@nik, @tanggalAbsen)";
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@nik", nik);
+                cmd.Parameters.AddWithValue("@tanggalAbsen", tanggalAbsen);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error saat eksekusi query: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private bool isDuplicateAttendance(string nik, DateTime tanggalAbsen)
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM absen WHERE NIK = @nik AND CONVERT(date, Tanggal_Absen) = CONVERT(date, @tanggalAbsen)";
+                cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@nik", nik);
+                cmd.Parameters.AddWithValue("@tanggalAbsen", tanggalAbsen);
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
